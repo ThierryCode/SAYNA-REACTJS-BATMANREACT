814df36c15f0f2ea46088f79563a3d591f3a225d
Andry Kamamy <tchithierry@gmail.com>
2023-09-19 17:58:59 +0300

    Mise parametrage du layout du site

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import { Home } from './pages/Home';
+import {Eshop} from './pages/ESHOP/Eshop'
+import {MonCompte} from './pages/MonCompte/components/MonCompte'
+import { Game } from './pages/Game';
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}/>
+          <Route path='/Eshop' element={<Eshop />}/>
+          <Route path='/MonCompte' element={<MonCompte />}/>
+          <Route path='/game' element={<Game />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/pages/MonCompte/components/MonCompte.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const MonCompte= () => {
+  return (
+    <div>MonCompte</div>
+  )
+}
