c152a3382a2a1c41fcbd5bea93ddc0d357b315a9
Andry Kamamy <tchithierry@gmail.com>
2023-09-19 17:13:57 +0300

    Ajout des fichier de components

---
+++ src/pages/ESHOP/Eshop.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Eshop = () => {
+  return (
+    <div>Eshop</div>
+  )
+}
---
+++ src/pages/MON_COMPTE/components/UserProfile/UserProfil.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const UserProfil = () => {
+  return (
+    <div>UserProfil</div>
+  )
+}
