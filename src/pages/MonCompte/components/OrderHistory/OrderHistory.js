2c954a45b0311c323590004220c0184e5c7a9ffc
Andry Kamamy <tchithierry@gmail.com>
2023-10-05 16:11:33 +0300

    Mise en ligne sur firebase du M V P

--- src/pages/MonCompte/components/OrderHistory/OrderHistory.js
+++ src/pages/MonCompte/components/OrderHistory/OrderHistory.js
@@ -1,14 +1,24 @@
-import React from 'react'
-
-export const OrderHistory = () => {
-  return (
-    <>
-      <div className="orderHistory">
-        <h3>Commande</h3>
-        <div className="Article">
-          
-        </div>
-      </div>
-    </>
-  )
-}
+// import React from 'react';
+// import { PRODUCTS } from '../../../ESHOP/products';
+// import { shopContext } from '../../../../context/shopContext';
+// import { CartItem } from '../../../ESHOP/Cart/cart-item';
+// export const OrderHistory = () => {
+//   const {cartItems, getSousTotal} = useContext(shopContext);
+//   return (
+//     <>
+//       <div className="orderHistory">
+//         <div className="commande">
+//         <h3>Commande</h3>
+//           <div className="article">
+            
+//           {PRODUCTS.map((product) => {
+//                 if (cartItems[product.id] !== 0){
+//                   return <CartItem data={product}/>
+//                 }
+//                 })}
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
